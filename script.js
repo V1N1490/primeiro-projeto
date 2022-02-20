f5ad5566440925287d2b800396fb23bead21ee54
V1N1490 <vinibarbosa490@gmail.com>
2022-02-20 07:03:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Vinicius Barbosa Alves
+var nome = "Vinicius Barbosa Alves"
+var anoDeNascimento = 2004
+var anoAtual = new Date().getFullYear()
+var idade = anoAtual - anoDeNascimento
+
+alert(`Boas vindas ao meu site ! me chamo ${nome} tenho ${idade} anos`)
